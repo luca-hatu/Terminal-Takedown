5ad75468de9ee2b06ab5c99685dad706d456548a
Luca-Matei HATU <lucamhatu@gmail.com>
2024-08-24 12:09:30 +0200

    added levels, score, increased difficulty

--- termina-takedown.py
+++ termina-takedown.py
@@ -5,9 +5,14 @@ import random
 PLAYER_CHAR = "^"
 BULLET_CHAR = "|"
 ENEMY_CHAR = "V"
+ENEMY_FAST_CHAR = "W"
+POWERUP_CHAR = "*"
 BULLET_SPEED = 0.05
-ENEMY_SPEED = 0.5
-ENEMY_SPAWN_RATE = 10
+ENEMY_SPEED = 0.1
+FAST_ENEMY_SPEED = 0.15
+POWERUP_SPAWN_RATE = 100
+ENEMY_SPAWN_RATE = 15
+PLAYER_SPEED = 2
 
 class Game:
     def __init__(self, stdscr):
@@ -17,7 +22,9 @@ class Game:
         self.player_y = self.sh - 2
         self.bullets = []
         self.enemies = []
+        self.powerups = []
         self.score = 0
+        self.level = 1
         self.game_over = False
         self.enemy_spawn_counter = ENEMY_SPAWN_RATE
 
@@ -30,7 +37,11 @@ class Game:
 
     def draw_enemies(self):
         for enemy in self.enemies:
-            self.stdscr.addch(enemy['y'], enemy['x'], ENEMY_CHAR)
+            self.stdscr.addch(enemy['y'], enemy['x'], enemy['char'])
+
+    def draw_powerups(self):
+        for powerup in self.powerups:
+            self.stdscr.addch(powerup['y'], powerup['x'], POWERUP_CHAR)
 
     def move_bullets(self):
         new_bullets = []
@@ -42,8 +53,13 @@ class Game:
 
     def move_enemies(self):
         for enemy in self.enemies:
-            enemy['y'] += 1
+            enemy['y'] += 1 if enemy['char'] == ENEMY_CHAR else 2
         self.enemies = [e for e in self.enemies if e['y'] < self.sh]
+
+    def move_powerups(self):
+        for powerup in self.powerups:
+            powerup['y'] += 1
+        self.powerups = [p for p in self.powerups if p['y'] < self.sh]
 
     def shoot_bullet(self):
         bullet = {'x': self.player_x, 'y': self.player_y - 1}
@@ -52,8 +68,15 @@ class Game:
     def spawn_enemy(self):
         if random.randint(0, self.enemy_spawn_counter) == 0:
             enemy_x = random.randint(0, self.sw - 1)
-            enemy = {'x': enemy_x, 'y': 0}
+            enemy_type = ENEMY_CHAR if random.random() > 0.5 else ENEMY_FAST_CHAR
+            enemy = {'x': enemy_x, 'y': 0, 'char': enemy_type}
             self.enemies.append(enemy)
+
+    def spawn_powerup(self):
+        if random.randint(0, POWERUP_SPAWN_RATE) == 0:
+            powerup_x = random.randint(0, self.sw - 1)
+            powerup = {'x': powerup_x, 'y': 0}
+            self.powerups.append(powerup)
 
     def handle_input(self):
         key = self.stdscr.getch()
@@ -68,9 +91,9 @@ class Game:
 
     def move_player(self, direction):
         if direction == "LEFT" and self.player_x > 0:
-            self.player_x -= 1
+            self.player_x -= PLAYER_SPEED
         elif direction == "RIGHT" and self.player_x < self.sw - 1:
-            self.player_x += 1
+            self.player_x += PLAYER_SPEED
 
     def check_collisions(self):
         new_bullets = []
@@ -90,9 +113,32 @@ class Game:
             if enemy['x'] == self.player_x and enemy['y'] == self.player_y:
                 self.game_over = True
 
+        for powerup in self.powerups:
+            if powerup['x'] == self.player_x and powerup['y'] == self.player_y:
+                self.powerups.remove(powerup)
+                self.activate_powerup()
+
+    def activate_powerup(self):
+        self.enemy_spawn_counter = max(ENEMY_SPAWN_RATE - 5, 5) 
+
     def draw_score(self):
-        score_str = f"Score: {self.score}"
+        score_str = f"Score: {self.score} Level: {self.level}"
         self.stdscr.addstr(0, 0, score_str)
+
+    def show_game_over(self):
+        self.stdscr.clear()
+        game_over_msg = "GAME OVER"
+        score_msg = f"Your Score: {self.score}"
+        restart_msg = "Press 'r' to Restart or 'q' to Quit"
+        self.stdscr.addstr(self.sh // 2 - 1, (self.sw - len(game_over_msg)) // 2, game_over_msg)
+        self.stdscr.addstr(self.sh // 2, (self.sw - len(score_msg)) // 2, score_msg)
+        self.stdscr.addstr(self.sh // 2 + 1, (self.sw - len(restart_msg)) // 2, restart_msg)
+        self.stdscr.refresh()
+
+    def level_up(self):
+        if self.score > 0 and self.score % 10 == 0:
+            self.level += 1
+            self.enemy_spawn_counter = max(ENEMY_SPAWN_RATE - (self.level * 2), 5)
 
     def game_loop(self):
         while not self.game_over:
@@ -100,19 +146,32 @@ class Game:
             self.handle_input()
             self.move_bullets()
             self.move_enemies()
+            self.move_powerups()
             self.spawn_enemy()
+            self.spawn_powerup()
             self.check_collisions()
+            self.level_up()
             self.draw_player()
             self.draw_bullets()
             self.draw_enemies()
+            self.draw_powerups()
             self.draw_score()
             self.stdscr.refresh()
             time.sleep(BULLET_SPEED)
 
+        self.show_game_over()
+        while True:
+            key = self.stdscr.getch()
+            if key == ord('r'):
+                self.__init__(self.stdscr)
+                self.game_loop()
+            elif key == ord('q'):
+                break
+
 def main(stdscr):
     curses.curs_set(0)
     stdscr.nodelay(1)
-    stdscr.timeout(50)
+    stdscr.timeout(50) 
 
     game = Game(stdscr)
     game.game_loop()
